68b5c9d2570e447b72b29ff54a073fd0fdf53a31
yuweiliu <yuweiliu@fareastone.com.tw>
2020-08-11 12:56:16 +0800

    Release 0.0.2

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ with open('LICENSE') as f:
     license_text = f.read()
 
 setup(
-    project_name='service-classification-model-training',
-    project_version='0.0.1',
+    project_name='service-data-eda-analysis',
+    project_version='0.0.2',
     description='In this project, we want to build the platform to do the evaluation.',
     long_description=readme_text,
     author='Simon Liu',
-    url='https://github.com/LiuYuWei/service-classification-model-training',
+    url='https://github.com/LiuYuWei/service-data-eda-analysis',
     license=license_text,
     packages=find_packages(exclude=('tests'))
 )
